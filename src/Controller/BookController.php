5e4045b43c7c616317b5426b8da2ec6a26d6db46
lmnghia911 <lmnghia911@gmail.com>
2022-02-24 11:50:29 +0700

    Delete function

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -10,12 +10,32 @@ use Symfony\Component\Routing\Annotation\Route;
 class BookController extends AbstractController
 {
     /**
-     * @Route("/book/{id}", name="book")
+     * @Route("/book/{id}", name="detail of book")
      */
     public function detail(Books $books): Response
     {
         return $this->render('book/detail.html.twig', [
             'book' => $books,
         ]);
+    }
+
+    /**
+     * @Route("/book/delete/{id}", name="delete book")
+     */
+    public function deleteBookById($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $bookRepo = $em->getRepository(Books::class);
+        $book = $bookRepo->find($id);
+        $em->remove($book);
+        $em->flush();
+
+        $this->addFlash(
+            'error',
+            'Todo deleted'
+        );
+
+        return $this->redirectToRoute('home');
+
     }
 }
